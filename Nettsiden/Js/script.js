ed02487883b40984d8c20e64b8351816ad74b400
Marius <671440@stud.hvl.no>
2024-10-03 12:34:01 +0200

    STARTEN

---
+++ Nettsiden/Js/script.js
@@ -0,0 +1,14 @@
+
+
+<nav class = "navbar"> 
+    <div class = "navdiv">
+        <div class = "logo"><a href = "Index.html">SpareGrisen</a></div>
+<ul> 
+    <li><a href="about.html"> About</a> </li>
+    <li><a href="products.html"> Products</a> </li>
+    <li><a href="contact.html"> Contact</a> </li>
+    <button><a href ="logIn.html">My profile</a> </button>
+   
+</ul>
+</div>
+</nav>
